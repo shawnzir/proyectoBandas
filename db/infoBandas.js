7e68f83fee0de854164d4e92237b066cf68f877d
Shawn Zir <szir@udesa.edu.ar>
2024-03-24 13:05:45 -0300

    cambie las imagenes de los artistas

--- db/infoBandas.js
+++ db/infoBandas.js
@@ -12,7 +12,7 @@ module.exports = [
         integrantes: 5,
         genero: "Rock",
         topCanciones: ["Sweet child o' mine", "November rain","Welcome to the jungle"],
-        cover: 'images/acDc.jpg',
+        cover: 'images/guns.jpg',
         id: 2,
         video: "https://www.youtube.com/watch?v=1w7OgIMMRc4"
     },{
@@ -20,7 +20,7 @@ module.exports = [
         integrantes: 5,
         genero: "Hip hop",
         topCanciones: ["Straight outta compton","Fuk da police","Boys in the hood"],
-        cover: 'images/eminem.jpg',
+        cover: 'images/nwa.jpg',
         id: 3,
         video: "https://www.youtube.com/watch?v=TMZi25Pq3T8"
     },{
@@ -28,7 +28,7 @@ module.exports = [
         integrantes: 8,
         genero: "Hip hop",
         topCanciones: ["C.R.E.A.M","Method man","Da mystery of chessboxin'"],
-        cover: 'images/acDc.jpg',
+        cover: 'images/wu.png',
         id: 4,
         video: "https://www.youtube.com/watch?v=PEnwXYJcSZc"
     },{
@@ -36,7 +36,7 @@ module.exports = [
         integrantes: 1,
         genero: "Rap",
         topCanciones: ["Es épico", "Maquiavéñico", "Jeremías 17:5"],
-        cover: 'images/eminem.jpg',
+        cover: 'images/canserbero.jpg',
         id: 5,
         video: "https://www.youtube.com/watch?v=sq6oc066w14"
     },{
@@ -44,7 +44,7 @@ module.exports = [
         integrantes: 1,
         genero: "Rap",
         topCanciones: ["Lose yourself", "Stan", "The real slim shady"],
-        cover: 'images/acDc.jpg',
+        cover: 'images/eminem.jpg',
         id: 6,
         video: "https://www.youtube.com/watch?v=gOMhN-hfMtY"
     },{
@@ -52,7 +52,7 @@ module.exports = [
         integrantes: 7,
         genero: "Cumbia",
         topCanciones: ["Me vas a extrañar","Me muero de amor","No te creas tan importante"],
-        cover: 'images/acDc.jpg',
+        cover: 'images/damas.jpg',
         id: 7,
         video: "https://www.youtube.com/watch?v=iTNdq-GOVXc"
     },{
@@ -60,7 +60,7 @@ module.exports = [
         integrantes: 2,
         genero: "Cumbia",
         topCanciones: ["Que locura fue enamorarme de ti","Cariño","La plazita"],
-        cover: 'images/eminem.jpg',
+        cover: 'images/lechu.jpg',
         id: 8,
         video: "https://www.youtube.com/watch?v=QDy3f_FAML8"
     },{
@@ -68,7 +68,7 @@ module.exports = [
         integrantes: 8,
         genero: "Reggae",
         topCanciones: ["Casi q' me pierdo","Sé q' el mar","Bastará"],
-        cover: 'images/acDc.jpg',
+        cover: 'images/cafres.jpg',
         id: 9,
         video: "https://www.youtube.com/watch?v=afBhdRSCMSQ"
     },{
@@ -76,7 +76,7 @@ module.exports = [
         integrantes: 8,
         genero: "Reggae",
         topCanciones:[ "True love","You and me","I don't wanna wait"],
-        cover: 'images/eminem.jpg',
+        cover: 'images/soja.jpg',
         id: 10,
         video: "https://www.youtube.com/watch?v=XqgWJ_yaU7E"
     }
